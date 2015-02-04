c7311d49fe07674588606681a412bc4ac30d2766
sirmartymoose <sirmartymoose@gmail.com>
2015-02-04 23:07:26 +0000

    added 2 owned sheets to welcome page. Still working on shared

---
+++ client/javascript/client_welcome.js
@@ -0,0 +1,81 @@
+if (Meteor.isClient) {
+  
+
+  Template.welcome.rendered = function () {
+      
+
+      
+      
+    $(document).ready(function(){
+          
+          
+        function welcome_getOwnedSheets(){
+            console.log("getOwnedSheets Called")
+          
+          results = sheetDefinitions.find({userId: Meteor.userId()}, {limit:2}).fetch()
+          return results
+          
+      }
+      
+        function welcome_showOwnedSheets(){
+            console.log("showOwnedSheets Called")
+            
+
+ 
+            
+            result_string = ""
+            results = welcome_getOwnedSheets()
+            $(results).each(function(x,y){
+                            sheetName = y['title']
+                             submitted = y['submitted']
+                             myUserId = y['_id']
+                            sheetURL = "/posts/" + myUserId
+                            theString = "<li class='list-group-item'>"+
+    				"<div class='row'>"+
+    					"<div class='col-md-4' >"+ 
+    						"<div class='list-group-item-heading'>"+
+    							"<h4><a href='" + sheetURL  + "'> "+ sheetName +"</a></h4>"+
+    						"</div>"+
+    					"</div> "+
+    					"<div class='col-md-4'>"+
+    					"	<div class='list-group-item-heading'>"+
+    					"		<h4>Saved</h4> "+
+    						"</div>"+
+    					"</div>"+
+    				"</div>"+
+    				"<div class='row'>"+
+    					"<div class='col-md-4' > "+
+    							
+    						
+    					"</div> "+
+    					"<div class='col-md-2'>"+
+    					"		Status "+
+    					"</div>"+
+    				"</div>"+
+    			"</li>"
+                
+                
+                result_string = result_string + theString
+                
+                
+                
+                
+            })
+            
+            return result_string   
+            
+        }
+          
+          console.log("welcome rendered and document ready")
+          
+          $("#sheetsYouOwn_results").append(welcome_showOwnedSheets())
+        
+          
+          
+          
+      })
+
+      
+  }
+
+}
