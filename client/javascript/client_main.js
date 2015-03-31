45ece14199338566b96b1896784dbfc9ecb65c17
sirmartymoose <sirmartymoose@gmail.com>
2015-03-31 16:06:34 +0000

    starting cleanup

--- client/javascript/client_main.js
+++ client/javascript/client_main.js
@@ -14,10 +14,8 @@ if (Meteor.isClient) {
 
   Template.loginButtons.rendered = function () {
       
-      console.log("CLIENT MAIN LOADED")
          
       $("#login-buttons-logout").click(function(){
-            console.log("LOGOUT CLICKED")
             dcLogout()
       })
       
